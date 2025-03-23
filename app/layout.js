5647a2660731a380c436b0303658f0f3518edcd1
jackiboi12 <satyamjakhmola99@gmail.com>
2025-03-23 17:25:47 +0530

    header.jsx and basic config of middleware.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
@@ -12,7 +13,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      baseTheme:dark,
+    }}>
       <html lang="en" suppressHydrationWarning>
         <body
           className={`${inter.className}`}>
