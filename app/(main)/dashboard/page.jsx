e601cf40ea64ea4eb28e0b2feb1e9a26fd4f61ba
jackiboi12 <satyamjakhmola99@gmail.com>
2025-03-25 12:08:34 +0530

    checkpoint

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
+
+export default async function IndustryInsightsPage() {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  return <div className="container mx-auto">IndustryInsightsPage</div>;
+}
