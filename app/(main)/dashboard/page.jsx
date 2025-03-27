cef67eb55e885e4bc0a097b5dc6da5bdc06679f1
jackiboi12 <satyamjakhmola99@gmail.com>
2025-03-27 20:21:59 +0530

    inngest function

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,6 +1,7 @@
 import { getIndustryInsights } from "@/actions/dashboard";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
+import DashboardView from "./_components/dashboard-view";
 
 export default async function DashboardPage() {
   const { isOnboarded } = await getUserOnboardingStatus();
